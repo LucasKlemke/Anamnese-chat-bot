1d43eac1d358ec61fa629a1e7223a0ceaed8a56e
Lucas Klemke <lucas.klemke@Outlook.com.br>
2024-11-02 17:15:55 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from configuration import model
+
+history = []
+
+
+def start_chat():
+    print("Conversa Iniciada")
+    
+    while True:
+
+        user_input = input("Você: ")
+
+        chat_session = model.start_chat(history=history)
+
+        response = chat_session.send_message(user_input)
+
+        # print(chat_session.history)
+
+        model_response = response.text
+        print(f"Resposta: {model_response}")
+        print("")
+        history.append({"role": "user", "parts": [user_input]})
+        history.append({"role": "model", "parts": [model_response]})
+
+
+
